7b76f23e9c8f9891eec229700bdaeb25b5201cb2
Srushti Patil <srushtipatil0167@gmail.com>
2025-07-11 23:26:42 +0530

    Add files via upload

---
+++ Pattern Printing/03_Pattern03_to print N.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int i=0,j=0,Num=0;
+
+   printf("\n Enter the Num:");
+   scanf("%d", &Num);
+
+
+
+   printf("--------------------\n");
+
+   for(i=1; i< Num ; i++)                  //For outer transversal( i th row)
+   {
+       for(j=1; j< Num; j++)               //For inner transversal(j th column)
+       {
+           if(j == 1  || j==Num || i==j )
+           {
+              printf(" * ");
+           }
+           else
+           {
+              printf("   ");
+           }
+       }
+    printf("\n");
+   }
+
+   printf("---------------------\n");
+
+   getch();
+   return 0;
+
+
+}
