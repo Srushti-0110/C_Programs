4702dd11fa0f1bab0a09add09f1dc854edfd0831
Srushti Patil <srushtipatil0167@gmail.com>
2025-07-12 12:43:19 +0530

    Pattern printing

---
+++ Pattern Printing/08_Right Lower Pattern printing.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int i=0,j=0,RC=0;
+
+   printf("\n Enter the Row and Column:");
+   scanf("%d", &RC);
+
+
+   printf("--------------------\n");
+
+   for(i=1; i<=RC ; i++)                  //For outer transversal( i th row)
+   {
+       for(j=1; j<=RC; j++)               //For inner transversal(j th column)
+       {
+           if(i+j>=(RC+1))
+           {
+              printf(" * ");
+           }
+           else
+           {
+              printf("   ");
+           }
+       }
+    printf("\n");
+   }
+
+   printf("---------------------\n");
+
+   getch();
+   return 0;
+
+
+}
